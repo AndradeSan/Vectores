5fa826d290ca8cace46b733f4d5fa8ec45c7c0b8
AndradeSan <santiagoandrade@gmail.com>
2018-11-22 09:26:53 -0500

    Bloque Try catch

--- AppVectores/AppVectores/Program.cs
+++ AppVectores/AppVectores/Program.cs
@@ -14,7 +14,38 @@ namespace AppVectores
             Console.WriteLine("Hola Mundo");
             Console.WriteLine("Hola Mundo GitHab");
             Console.WriteLine("Probando Control de Versiones");
+
+            Console.WriteLine("Resultado Potencia: {0}",ExponenteFor(3,4));
+            try
+            {
+                Console.WriteLine("-5 Elevado a la 2 {0}", ExponenteFor(-5, 2));
+            }catch(Exception ex)
+            {
+                Console.WriteLine("Error:" + ex.Message.ToString());
+
+            }
+            Console.WriteLine("2 Elevado a la 5 {0}", ExponenteFor(2,5));
             Console.ReadKey();
+     
         }
+        public static int ExponenteFor(int x, int y)
+        {
+            int acum = 1;
+            if (x <= 0)
+            {
+                throw new Exception("X debe ser Positivo...");
+            }
+            if (y <= 0)
+            {
+                throw new Exception("Y debe ser Positivo...");
+            }
+            // if (x > 1 && y > 1)
+            for (int i = 1; i <= y; i++)
+                    acum *= x;
+            return (acum);
+
+        }
+
+
     }
 }
