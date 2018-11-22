a4668319d910550bbc966d7b37ac3320af139bae
AndradeSan <santiagoandrade@gmail.com>
2018-11-22 09:58:53 -0500

    Con 2 Aplicaciones

--- AppVectores/AppVectores/Program.cs
+++ AppVectores/AppVectores/Program.cs
@@ -15,7 +15,7 @@ namespace AppVectores
             Console.WriteLine("Hola Mundo GitHab");
             Console.WriteLine("Probando Control de Versiones");
 
-            Console.WriteLine("Resultado Potencia: {0}",ExponenteFor(3,4));
+            Console.WriteLine("{0} Elevado a {1} = {2}",3,4,ExponenteFor(3,4));
             try
             {
                 Console.WriteLine("-5 Elevado a la 2 {0}", ExponenteFor(-5, 2));
@@ -25,6 +25,7 @@ namespace AppVectores
 
             }
             Console.WriteLine("2 Elevado a la 5 {0}", ExponenteFor(2,5));
+            Console.WriteLine("la tota 5 equivale a{0}",Calificacion(5));
             Console.ReadKey();
      
         }
@@ -43,6 +44,27 @@ namespace AppVectores
             for (int i = 1; i <= y; i++)
                     acum *= x;
             return (acum);
+
+        }
+        public static String Calificacion(double c)
+        {
+            int x = 2;
+            if (c > 0 && c < 5)
+                return "Suspenso";
+            else
+                 if (c > 4 && c < 6.6)
+                return "aprovado";
+            else
+                 if (c > 6.4 && c < 8.6)
+                return "Notable";
+            else
+                 if (c > 8.4 && c < 10)
+                return "sobresaliente";
+            else
+                 if (c == 10)
+                return "Matricula de Honor";
+            else
+                return "Fuera de Rango";
 
         }
 
